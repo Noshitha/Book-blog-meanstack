92b642f5b4a4210b74147fda909522963e419bd4
Noshitha <noshithanoshi@gmail.com>
2021-06-17 12:11:41 +0530

    heroku

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ book.rating="4/5"
 book.review="Sense and Sensibility is a richly rewarding story of manners from one of the world's greatest novelists. Full of feeling, humor, and beautifully realized characters, this book is treasured by teens and adults who enjoy a complex romantic page-turner."
 app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/homepage.html')) //we can also use res.sendFile and res.json accordingly
 app.get('/login', (req, res) => res.send('Login Here ...'))
-app.get('/resgister', (req, res) => res.send('Register Here'))
+app.get('/register', (req, res) => res.send('Register Here'))
 app.get('/data', (req, res) => res.json(book))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
